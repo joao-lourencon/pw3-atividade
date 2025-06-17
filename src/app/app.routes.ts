57c934049a006a62c2bdd05a9e370cb97cbf9bdd
joao-lourencon <joaolouren478@gmail.com>
2025-06-17 11:48:33 -0300

    rotas e cabecalho

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { FaqPageComponent } from './faq-page/faq-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { NotfoundPageComponent } from './notfound-page/notfound-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: "login", pathMatch: 'full'},
+    {path: 'login', component: LoginPageComponent},
+    {path: 'home', component: HomePageComponent},
+    {path: 'duvidas', component: FaqPageComponent},
+    {path: 'sobre', component: AboutPageComponent},
+
+    {path: '**', component: NotfoundPageComponent}
+];
